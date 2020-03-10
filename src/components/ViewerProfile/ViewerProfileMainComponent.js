eef36ea31abfdd68105d02409534d452b6898b35
shoerl <hoerl.s@husky.neu.edu>
2020-03-10 13:22:03 -0400

    a

---
+++ src/components/ViewerProfile/ViewerProfileMainComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class ViewerProfileMainComponent extends React.Component {
+
+    render() {
+        return(
+            <div className="row"></div>
+            
+        )
+    }
+}
+export default ViewerProfileMainComponent
---
+++ src/containers/ViewerProfile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ViewerProfileMainComponent from "../components/ViewerProfile/ViewerProfileMainComponent";
+
+class ViewerProfile extends React.Component {
+
+    render() {
+        return(
+        <ViewerProfileMainComponent/>)
+    }
+
+}
+
+export default ViewerProfile
