5b3ae170c3e94d4b8710ee38d114d51ff5f1f18a
shoerl <hoerl.s@husky.neu.edu>
2020-04-19 01:17:12 -0400

    ah

--- src/components/ViewerProfile/LogoBar.js
+++ src/components/ViewerProfile/LogoBar.js
@@ -22,6 +22,7 @@ class LogoBar extends React.Component {
 
           <div className="text-right align-middle ml-2">
               <div className="row float-right mr-1">
+                  {console.log(this.props)}
                   <h5 className="text-center mr-2 d-none d-sm-block"> Logged in as {this.props.username} </h5>
                   {/*{this.props.username.charAt(0).toUpperCase() + this.props.username.slice(1)} </h5>*/}
 
