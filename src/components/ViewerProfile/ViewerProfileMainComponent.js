f4be341eda633b281f152760f8972d1361f188b5
Nate Smith <nateps313@gmail.com>
2020-04-21 20:21:51 -0400

    added advertiser redirect from main page

--- src/components/ViewerProfile/ViewerProfileMainComponent.js
+++ src/components/ViewerProfile/ViewerProfileMainComponent.js
@@ -4,81 +4,84 @@ import './Styling.css';
 import FollowListComponent from "./FollowListComponent";
 import WatchingGridComponent from "./WatchingGridComponent";
 import {connect} from "react-redux";
-import {Provider} from "react-redux";
 import "../../../node_modules/font-awesome/css/font-awesome.css"
 import userService from "../../services/UserService"
 import {loginUser, logout} from "../../actions/userActions";
-
+import AdvertiserPageComponent from "../Advertiser/AdvertiserPageComponent";
 
 class ViewerProfileMainComponent extends React.Component {
 
-  state = {
-
-  };
-
+  state = {};
 
   componentDidMount() {
-      this.props.profileRetrieve();
-      return;
+    this.props.profileRetrieve();
+    return;
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-     if (prevProps.user.id !== this.props.user.id) {
-         this.props.profileRetrieve();
-     }
+    if (prevProps.user.id !== this.props.user.id) {
+      this.props.profileRetrieve();
+    }
   }
 
-    logout = () => {
-      this.props.logout();
-      this.props.history.push("/login");
+  logout = () => {
+    this.props.logout();
+    this.props.history.push("/login");
   }
 
-
-    render() {
+  render() {
     return (
 
-          <div className="body">
-            <LogoBar
-                logout={this.logout}
-                uid={this.props.uid}
-                history={this.props.history}
-                username={this.props.user.username}
-                userId={this.props.user.id}/>
-
-            <div className="row mt-2 ml-1 mr-1 mb-2">
-              <div className="col">
-                <div className="m-2">
-                  <WatchingGridComponent
-                      uid={this.props.uid}
-                      user={this.props.user}
-                      history={this.props.history}/>
-                </div>
+        <div className="body">
+          <LogoBar
+              logout={this.logout}
+              uid={this.props.uid}
+              history={this.props.history}
+              username={this.props.user.username}
+              userId={this.props.user.id}/>
+          {this.props.user.userType !== "Advertiser" &&
+          <div className="row mt-2 ml-1 mr-1 mb-2">
+            <div className="col">
+              <div className="m-2">
+                <WatchingGridComponent
+                    uid={this.props.uid}
+                    user={this.props.user}
+                    history={this.props.history}/>
               </div>
-                {this.props.user.id !== -1 &&
-              <div className="col-3 d-none d-sm-block">
-                <FollowListComponent
-                    user={this.props.user}/>
-              </div>}
             </div>
-
+            {this.props.user.id !== -1 &&
+            <div className="col-3 d-none d-sm-block">
+              <FollowListComponent
+                  user={this.props.user}/>
+            </div>
+            }
           </div>
+          }
 
+          {this.props.user.userType === "Advertiser" &&
+            <AdvertiserPageComponent
+              uid={this.props.uid}
+              user={this.props.user}
+              history={this.props.history}
+            />
+          }
+        </div>
     )
   }
 }
 
 const stateToPropertyMapper = (state) => ({
-    media: state.media.media,
-    user: state.user.user
+  media: state.media.media,
+  user: state.user.user
 });
 
 const dispatcherToPropertyMapper = (dispatch) => ({
-    logout: () =>
-        userService.logoutUser()
-            .then(response => dispatch(logout())),
-    profileRetrieve: () =>
-        userService.profileRetrieve()
-            .then(user => dispatch(loginUser(user)))
+  logout: () =>
+      userService.logoutUser()
+      .then(response => dispatch(logout())),
+  profileRetrieve: () =>
+      userService.profileRetrieve()
+      .then(user => dispatch(loginUser(user)))
 
 });
 
